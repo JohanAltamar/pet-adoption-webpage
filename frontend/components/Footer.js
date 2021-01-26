7d5b289044b18cc136b044138014779b57fdc4c9
Johan Altamar <johan.altamar.dev@gmail.com>
2021-01-26 16:24:08 -0500

    footer updated

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,20 +2,23 @@ const Footer = () => {
   return (
     <div className="flex justify-between m-6">
       <p className="text-xs font-semibold text-gray-600">
-        Strapi Starter Next.js E-commerce
+        Huellitas de amor - Desarrollado por{" "}
+        <a href="https://johanaltamar.com" target="_blank">
+          Johan Altamar
+        </a>
       </p>
       <div className="flex gap-3 ml-4">
         <a href="https://twitter.com/strapijs" className="max-w-xs ml-4">
-          <img src="/twitter.svg" alt="Twitter" />
+          <img src="/twitter.svg" alt="Twitter" width="20px" />
         </a>
         <a href="https://facebook.com/strapijs" className="ml-3">
-          <img src="/facebook.svg" alt="Facebook" />
+          <img src="/facebook.svg" alt="Facebook" width="20px" />
         </a>
         <a
           href="https://github.com/strapi/strapi-starter-next-ecommerce"
           className="ml-3"
         >
-          <img src="/github.svg" alt="GitHub" />
+          <img src="/instagram.svg" alt="Instagram" width="20px" />
         </a>
       </div>
     </div>
