3c66a4385826a85cad7a8819a502de352af6f237
Johan Altamar <johan.altamar.dev@gmail.com>
2021-05-16 13:00:45 -0500

    change products by pets

--- components/LatestPosts.js
+++ components/LatestPosts.js
@@ -1,29 +1,25 @@
 import Link from "next/link";
-import { getStrapiMedia } from "../utils/medias";
 
-const ProductsList = ({ products }) => {
+const PetsList = ({ pets }) => {
   return (
     <div className="px-10 sm:px-16 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-8">
-      {products.map((_product) => (
+      {pets.map((_pet) => (
         <div
-          key={_product.id}
+          key={_pet.slug}
           className="border rounded-lg bg-gray-100 hover:shadow-lg shadow-md"
         >
-          <Link href={`/products/${_product.slug}`}>
+          <Link href={`/fundaciones/${_pet.foundation.slug}/${_pet.slug}`}>
             <a>
               <div className="p-2 rounded-lg">
                 <h4 className="mt-1 font-semibold text-base text-center leading-tight truncate text-gray-700">
-                  {_product.title}
+                  {_pet.name}
                 </h4>
-                {/* <div className="mt-1 text-sm text-gray-700">
-                  {_product.description}
-                </div> */}
               </div>
               <div className="rounded-t-lg bg-white pt-2 pb-2">
                 <img
                   className="crop mx-auto"
-                  src={getStrapiMedia(_product.image.formats.thumbnail.url)}
-                  alt={_product.title}
+                  src={_pet.imageUrl}
+                  alt={_pet.name}
                 />
               </div>
             </a>
@@ -34,4 +30,4 @@ const ProductsList = ({ products }) => {
   );
 };
 
-export default ProductsList;
+export default PetsList;
