7b989029c6e51669ded10a03d728dc25ccf33f90
Johan Altamar <johan.altamar.dev@gmail.com>
2021-05-17 16:50:08 -0500

    update pet page

--- pages/fundaciones/[slug]/[pid].js
+++ pages/fundaciones/[slug]/[pid].js
@@ -1,9 +1,70 @@
-const FoundationPetPage = () => {
-  return (
-    <div>
-      <h1>Foundation Pet Page</h1>
-    </div>
-  );
+import { gql } from "@apollo/client";
+import client from "../../../apollo-client";
+import Container from "../../../components/petPageComponents/Container";
+
+const FoundationPetPage = ({ info }) => {
+  return <Container info={info} />;
 };
 
 export default FoundationPetPage;
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      query Pets {
+        pets {
+          slug
+          foundation {
+            slug
+          }
+        }
+      }
+    `,
+  });
+
+  const paths = data.pets.map((pet) => ({
+    params: { pid: pet.slug, slug: pet.foundation.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { data } = await client.query({
+    query: gql`
+      query Pets($slug: String!) {
+        pets(where: { slug: $slug }) {
+          id
+          description
+          imageUrl
+          name
+          slug
+          type
+          age
+          gender
+          city
+          department
+          foundation {
+            id
+            description
+            form
+            imageUrl
+            name
+            slug
+          }
+        }
+      }
+    `,
+    variables: { slug: params.pid },
+  });
+
+  return {
+    props: {
+      info: { ...data.pets[0] },
+    },
+    revalidate: 1,
+  };
+}
